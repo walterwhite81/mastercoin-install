d3fa332bcbef82c39c1c4d96411caf4949b49ac1
walterwhite81 <walter81@protonmail.com>
2018-09-25 15:34:11 +0200

    install: add more progress visibility

--- mc_install.sh
+++ mc_install.sh
@@ -187,19 +187,21 @@ fi
 }
 
 function prepare_system() {
-echo -e "Prepare the system to install ${GREEN}$COIN_NAME${NC} master node."
-apt-get update >/dev/null 2>&1
-DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null 2>&1
-DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -qq upgrade >/dev/null 2>&1
-apt install -y software-properties-common >/dev/null 2>&1
-echo -e "${GREEN}Adding bitcoin PPA repository"
-apt-add-repository -y ppa:bitcoin/bitcoin >/dev/null 2>&1
-echo -e "Installing required packages, it may take some time to finish.${NC}"
-apt-get update >/dev/null 2>&1
-apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
-build-essential libtool autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev \
-libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git wget curl libdb4.8-dev bsdmainutils libdb4.8++-dev \
-libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev unzip libzmq5 >/dev/null 2>&1
+  echo -e "Preparing the system to install ${GREEN}$COIN_NAME${NC} master node."
+  apt-get update >/dev/null 2>&1
+  echo -e "${GREEN}Refreshing available system packages and updates.${NC}"
+  DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null 2>&1
+  echo -e "${GREEN}Updating system.${NC}"
+  DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -qq upgrade >/dev/null 2>&1
+  apt install -y software-properties-common >/dev/null 2>&1
+  echo -e "${GREEN}Adding the bitcoin repository.${NC}"
+  apt-add-repository -y ppa:bitcoin/bitcoin >/dev/null 2>&1
+  echo -e "${GREEN}Installing required additional packages, this may take some time to finish depending on the system specs. ${RED}Don't press any keys.${NC}"
+  apt-get update >/dev/null 2>&1
+  apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
+  build-essential libtool autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev \
+  libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git wget curl libdb4.8-dev bsdmainutils libdb4.8++-dev \
+  libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev unzip libzmq5 >/dev/null 2>&1
 if [ "$?" -gt "0" ];
   then
     echo -e "${RED}Not all required packages were installed properly. Try to install them manually by running the following commands:${NC}\n"
@@ -241,7 +243,6 @@ function setup_node() {
 
 ##### Main #####
 clear
-
 checks
 prepare_system
 download_node
